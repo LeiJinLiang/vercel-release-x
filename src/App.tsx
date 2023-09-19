8d62b57a3e6687e1e7a9bd29d4e4497f446c9268
andy <466179464@qq.com>
2023-09-19 22:29:41 +0800

    test: 💍 bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { useEffect, useState } from 'react';
-import { API_HOST } from './config';
-import './App.css';
+import { useEffect, useState } from "react";
+import { API_HOST } from "./config";
+import "./App.css";
 
 declare var window: any;
 function App() {
-
   const [files, setFiles] = useState<FileList | null>(null);
-  const [jsonData, setJsonData] = useState<string>('');
+  const [jsonData, setJsonData] = useState<string>("");
 
   const onSubmit = () => {
     if (!files) {
@@ -14,52 +13,76 @@ function App() {
     }
 
     const formData = new FormData();
-    formData.append('file', files[0]);
+    formData.append("file", files[0]);
 
     fetch(`${API_HOST}/upload`, {
-      method: 'POST',
+      method: "POST",
       body: formData,
     })
-      .then(resp => resp.json())
-      .then(data => {
+      .then((resp) => resp.json())
+      .then((data) => {
         setJsonData(JSON.stringify(data, null, 2));
       });
   };
 
-
   useEffect(() => {
-    console.log('window', window.wx);
-    console.log('window.wx.miniProgram', window?.wx?.miniProgram);
+    console.log("window", window.wx);
+    console.log("window.wx.miniProgram", window?.wx?.miniProgram);
   }, []);
 
   const onTest = () => {
-    console.log('click mini');
+    console.log("click mini");
     if (window.wx && window.wx.miniProgram) {
-      console.log('window', window);
-      window.wx.miniProgram.navigateTo({ url: '/pages/index/index?needAuth=true' });
+      console.log("window", window);
+      window.wx.miniProgram.navigateTo({
+        url: "/pages/index/index?needAuth=true",
+      });
     }
   };
 
-
   const onDebugger = () => {
-    window.open('https://www.baidu.com');
-  }
+    window.open("https://www.baidu.com");
+  };
 
-  const onDebugger2 = () => {
-     window.location.href = 'https://www.baidu.com';
-  }
+  const onDebugger2 = (e: any) => {
+    e.preventDefault();
+    window.location.href = "https://www.baidu.com";
+  };
 
   return (
     <div className="App">
-      <input type='file' onChange={(e) => {
-        setFiles(e.target.files);
-      }} />
+      <input
+        type="file"
+        onChange={(e) => {
+          setFiles(e.target.files);
+        }}
+      />
       <button onClick={onSubmit}>upload</button>
       <br />
       <button onClick={onTest}>test mini app</button>
 
-       <button onClick={onDebugger}>debugger</button>
-         <button onClick={onDebugger2}>debugger2</button>
+      <button onClick={onDebugger}>debugger</button>
+      <button onClick={onDebugger2}>debugger2</button>
+      <a href="https://www.lilith.com/termofservice" target="_blank">
+        《用户协议》
+      </a>
+      <a href="https://www.lilith.com/termofservice">《用户协议》2</a>
+
+      <span
+        onClick={(e: any) => {
+          e.preventDefault();
+          window.location.href = "https://www.lilithgames.com/privacy";
+        }}
+      >
+        《隐私政策》
+      </span>
+      <span
+        onClick={(e: any) => {
+          window.location.href = "https://www.lilithgames.com/privacy";
+        }}
+      >
+        《隐私政策2》
+      </span>
     </div>
   );
 }
